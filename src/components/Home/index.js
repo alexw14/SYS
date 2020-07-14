a26393080b52870a53aece9e8a7b1cd709e40955
Alex Wong <alexw14@gmail.com>
2020-07-13 23:27:25 -0700

    update height to use window.innerHeight

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,11 +5,13 @@ import './Home.css';
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div class="app-container">
         {/* <Navbar /> */}
-        <div className="hero-img"></div>
+        <div className="hero-img" style={{ height: window.innerHeight }}></div>
         <div className="wrapper">
-          <div className="main">Stacey <span>Yellow</span> Studio</div>
+          <div className="main">
+            Stacey <span>Yellow</span> Studio
+          </div>
           <div className="desc">
             I am a designer who loves to work hands on.
           </div>
