94594fce57b9205926fcfd92311457a0f161fa2c
Alex Wong <alexw14@gmail.com>
2020-07-13 22:53:31 -0700

    update home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
-import Navbar from '../Navbar';
+import React from 'react';
+// import Navbar from '../Navbar';
 import './Home.css';
 
-class Home extends Component {
+class Home extends React.Component {
   render() {
     return (
       <div>
-        <Navbar />
+        {/* <Navbar /> */}
         <div className="hero-img"></div>
+        <div className="wrapper">
+          <div className="main">Stacey <span>Yellow</span> Studio</div>
+          <div className="desc">
+            I am a designer who loves to work hands on.
+          </div>
+        </div>
       </div>
     );
   }
